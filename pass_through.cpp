b4149f42cf8f21876fdc96c7f22281d243c7d919
ExpressLens <expresslens@proton.me>
2022-08-21 12:57:41 +0000

    add french translation

--- pass_through.cpp
+++ pass_through.cpp
@@ -4,4 +4,13 @@
 // PCL specific includes
 #include <boost/bind.hpp>
 #include <sensor_msgs/PointCloud2.h>
-#include <pcl_conversions/
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/sample_consensus/method_types.h>
