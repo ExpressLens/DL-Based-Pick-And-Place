7ea00bd95cf19dc038068022a0ea443959d9d934
ExpressLens <expresslens@proton.me>
2023-03-13 09:13:13 +0000

    nilfs2: fix fix very long mount time issue

--- pass_through.cpp
+++ pass_through.cpp
@@ -95,4 +95,14 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg){
 if(endCallback1 == 0){
     
  // Container for original & filtered data
-  pcl::PCLPointCloud2* cloud = new p
+  pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2;
+  pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
+  pcl::PCLPointCloud2* cloud_filtered = new pcl::PCLPointCloud2;
+  pcl::PCLPointCloud2Ptr cloudFilteredPtr (cloud_filtered);
+
+
+  // Convert to PCL data type
+  pcl_conversions::toPCL(*cloud_msg, *cloud);
+
+
+  // Perform 
