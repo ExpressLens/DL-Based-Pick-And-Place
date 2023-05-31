07126c21d1f10514a9eacdbaab9c1cbd5e5a18aa
ExpressLens <expresslens@proton.me>
2023-05-31 20:17:46 +0000

    Initial create

--- pass_through.cpp
+++ pass_through.cpp
@@ -201,4 +201,11 @@ for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (
     camera_matrix <<  547.471175, 0.000000, 313.045026, 0.000000, 547.590335, 237.016225, 0.000000, 0.000000, 1.000000;
     
     //puts the clusters into cloud_cluster
-    for (std::vector<int>::const_iterator pit = it->i
+    for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit)
+    {
+        cloud_cluster->points.push_back (xyzCloudPtrRansacFiltered->points[*pit]); 
+    }    
+    cloud_cluster->width = cloud_cluster->points.size (); 
+    cloud_cluster->height = 1; 
+    cloud_cluster->is_dense = true; 
+    //std::cout << "PointCloud repre
